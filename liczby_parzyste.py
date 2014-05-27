abca8083e34a311cf52d8a327d29697e9ec9b947
Adam BOCZULA <adam.boczula@hotmail.com>
2014-05-27 14:53:23 +0200

    Liczby parzyste

---
+++ liczby_parzyste.py
@@ -0,0 +1,10 @@
+# Podstawy algorytmow
+# Autor: Adam BOCZULA
+# Data: 27.05.2014 r.
+
+# klasa Liczby_parzyste
+
+class Liczby_parzyste(Object):
+    __init__(self):
+        None
+
---
+++ test_liczby_parzyste.py
@@ -0,0 +1,17 @@
+# Testy to liczb parzystych
+# Autor: Adam BOCZULA
+# Data: 27.05.2014 r.
+
+# importy
+import unittest
+from liczby_parzyste import Liczby_parzyste
+
+class TestCutter(unittest.TestCase):
+    @unittest.skip('Not yet implemented')
+    def test_spawn_object_Liczby_parzyste(self):
+        self.assertIsInstance(liczby_parzyste, Liczby_parzyste)
+
+if __name__ == "__main__":
+
+# koniec pliku
+
