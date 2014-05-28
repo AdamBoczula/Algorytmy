f121f583e381c50275b4089e49e978d6efabfdc7
Adam BOCZULA <adam.boczula@hotmail.com>
2014-05-29 00:11:52 +0200

    First TDD find even numbers in range <a, b>

--- liczby_parzyste.py
+++ liczby_parzyste.py
@@ -4,7 +4,23 @@
 
 # klasa Liczby_parzyste
 
-class Liczby_parzyste(Object):
-    __init__(self):
+class Liczby_parzyste:
+    def __init__(self):
         None
+    
+    def wyznacz_parzyste(self, a, b):
+        result = []
+        i = a
+        if(a >= b):
+            return -1
+        while(i < b):
+            if i%2 == 0 and i != 0:
+                result.append(i)
+            i = i+1
+        return result
 
+if __name__ == "__main__":
+    l = Liczby_parzyste()
+    a=-10
+    b=21
+    print(l.wyznacz_parzyste(a,b))
--- test_liczby_parzyste.py
+++ test_liczby_parzyste.py
@@ -7,11 +7,55 @@ import unittest
 from liczby_parzyste import Liczby_parzyste
 
 class TestCutter(unittest.TestCase):
-    @unittest.skip('Not yet implemented')
+    # test nr 1
+    # @unittest.skip('Not yet implemented')
     def test_spawn_object_Liczby_parzyste(self):
+        liczby_parzyste = Liczby_parzyste()
         self.assertIsInstance(liczby_parzyste, Liczby_parzyste)
+    
+    # test nr 2
+    def test_a_rowne_b(self):
+        liczby_parzyste = Liczby_parzyste()
+        a = 0
+        b = 0
+        self.assertEqual(-1,liczby_parzyste.wyznacz_parzyste(a,b))
+
+    # test nr 3
+    def test_a_wieksze_b(self):
+        liczby_parzyste = Liczby_parzyste()
+        a = 1
+        b = 0
+        self.assertEqual(-1,liczby_parzyste.wyznacz_parzyste(a,b))
+
+    # test nr 4
+    def test_zwroc_brak_parzystych_z_przedzialu_(self):
+        l = Liczby_parzyste()
+        a = 1
+        b = 2
+        self.assertEqual([],l.wyznacz_parzyste(a,b))
+
+    # test nr 5
+    def test_zwroc_jedna_parzysta_z_przedzialu(self):
+        l = Liczby_parzyste()
+        a = 1
+        b = 3
+        self.assertEqual([2],l.wyznacz_parzyste(a,b))
+
+    # test nr 6 
+    def test_zwroc_wszystkie_parzyste_z_przedzialu(self):
+        l = Liczby_parzyste()
+        a = 1
+        b = 5
+        self.assertEqual([2,4],l.wyznacz_parzyste(a,b))
+
+    # test nr 7
+    def test_nie_bierz_zero_za_parzysta(self):
+        l = Liczby_parzyste()
+        a = -1
+        b = 1
+        self.assertEqual([],l.wyznacz_parzyste(a,b))
 
 if __name__ == "__main__":
+    unittest.main()
 
 # koniec pliku
-
