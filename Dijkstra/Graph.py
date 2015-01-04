0b72c619a487079782fc20cd0dcc20dabbfe4c73
AdamBoczula <adam@adam.boczula@hotmail.com>
2015-01-04 18:50:11 +0100

    tworzenie grafu done.'

--- Dijkstra/Graph.py
+++ Dijkstra/Graph.py
@@ -6,21 +6,13 @@ class Graph:
   
     vertex = {}
 
-    def addEdge(self, line):
-        vertexId = self.findVertex(line[0])
-        firstVertex = line[vertexId]
-        if self.findVertex(vertexId) == False:
-            self.vertex[int(firstVertex)] = []
-        self.vertex[(int(firstVertex))].append({int(line[1]) : int(line[2])})
 
-    def findVertex(self, vertexId):
-        if self.vertex.keys() == []:
-            return False
-        for i in range(len(self.vertex.keys())):
-            if self.vertex.keys()[i] is vertexId and self.vertex.keys() is not []: 
-                return i
-            else:
-                return False
+    def addEdge(self, line):
+        try:
+            self.vertex[int(line[0])].append({int(line[1]) : int(line[2])})
+        except KeyError:
+            self.vertex[int(line[0])] = [{int(line[1]) : int(line[2])}]
+
 
     def createGraphFromStr(self, graphStr):
         graphList = graphStr.split()
@@ -34,24 +26,29 @@ class Graph:
                 lineOfGraphTxt.append(i)
         return self.vertex.keys()
 
+
     def getNumberOfVertex(self):
         return len(self.vertex.keys())
 
+
     def getNumberOfEdges(self):
-        retValue = 0
-        for element in self.vertex.keys():
-            retValue += len(element)
-        return retValue
+        retVal = 0
+        for elem in self.vertex.values():
+            retVal += len(elem)
+        return retVal
+
 
     def getVertex(self):
         return self.vertex.keys()
 
-    def getConnectFromVertex(self, vertexId):
-        # return self.vertex[vertexId].keys()
-        retVal = []
-        for elem in vertex[vertexId]:
-            retVal.append()
-        print(self.vertex[vertexId][0].keys())
 
-    def getEdgeFromVToVx(self, v1, v2):
-        return self.vertex[v1][v2]
+    def getConnectFromVertex(self, vertexId):
+        retVal = []
+        for elem in self.vertex[vertexId]:
+            retVal.append(elem.keys())
+        return retVal
+
+
+    def getEdgeFromVToVx(self, v1, v2): 
+        matches = [ x for x in self.vertex[v1] if [v2] == x.keys() ]
+        return matches[0].values()
--- Dijkstra/test_graph_pytest.py
+++ Dijkstra/test_graph_pytest.py
@@ -6,10 +6,9 @@ from Graph import Graph
 class testGraph(unittest.TestCase):
   
     def setUp(self):
-        print 'fikszczur graph'
         self.graph = Graph()
     
-    @unittest.skip("jeszcze nie mamb")
+    # @unittest.skip("jeszcze nie mamb")
     def testCreateSpecyficGraphFromFile(self):
         readFromFileMock = mock.Mock(return_value = """ 
             0 1 3\n
@@ -24,9 +23,7 @@ class testGraph(unittest.TestCase):
         readFromFileMock("testGraf.txt") #gen
         readFromFileMock.assert_called_once_with("testGraf.txt") #sciaganie werifikatora
         self.graph.createGraphFromStr(readFromFileMock.return_value)
-        print(self.graph.vertex)
         self.assertEqual(6, self.graph.getNumberOfVertex())
-        print(self.vertex.keys())
         self.assertEqual(9, self.graph.getNumberOfEdges())
 
     # @unittest.skip("jeszcze nie mamb")
@@ -34,8 +31,8 @@ class testGraph(unittest.TestCase):
         s = [1, 2, 3]
         self.graph.addEdge(s)
         self.assertEqual([1], self.graph.getVertex())
-        self.assertEqual([2], self.graph.getConnectFromVertex(1))
-        self.assertEqual(3, self.graph.getEdgeFromVToVx(1, 2))
+        self.assertEqual([[2]], self.graph.getConnectFromVertex(1))
+        self.assertEqual([3], self.graph.getEdgeFromVToVx(1, 2))
 
 
 if __name__ == '__main__':
